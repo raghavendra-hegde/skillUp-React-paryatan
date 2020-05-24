3017b02efea3f9da38e465d8fc0125114948d6e9
raghavendralhegde <raghavendra.hegde@ymedialabs.com>
2020-05-24 22:28:35 +0530

    Fixed issue with single venue call

--- src/pages/SingleFullVenue/Point.js
+++ src/pages/SingleFullVenue/Point.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function Point(props){
     
-    const descObj = props.pointDesc.find((point)=> point.pointTitle === props.point)
-
+    let descObj = props.pointDesc.find((point)=> point.pointTitle === props.point)
+    console.log(descObj)
     return(
+        
         <div>
-            <div className="point-title">{props.point}</div>
-            <div className="point-desc">{descObj.text}</div>
+            <div className="point-title">{props.pointTitle}</div>
+            {/* <div className="point-desc">{descObj.text}</div>  */}
         </div>
     )
 }
